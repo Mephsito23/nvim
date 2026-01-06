8d07eb6c5e95d55a2fe14b457222380468bb5e21
Mephisto <xiezefu@outlook.com>
2026-01-06 11:13:17 +0800

    修改快捷键

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local map = vim.api.nvim_set_keymap
-
 -- 复用 opt 参数
 local opt = {
 	noremap = true,
@@ -10,39 +8,41 @@ local opt = {
 }
 
 -- visual模式下缩进代码
-map("v", "<", "<gv", opt)
-map("v", ">", ">gv", opt)
+vim.keymap.set("v", "<", "<gv", opt)
+vim.keymap.set("v", ">", ">gv", opt)
 
 -- nvim-tree alt + m 键打开关闭tree
-map("n", "<leader>m", ":NvimTreeToggle<CR>", opt)
-map("n", "<leader>J", ":NvimTreeFindFile<CR>", opt)
+vim.keymap.set("n", "<leader>m", ":NvimTreeToggle<CR>", opt)
+vim.keymap.set("n", "<leader>J", ":NvimTreeFindFile<CR>", opt)
 
 -- BufferLine快捷键设置
-map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
-map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
-map("n", "<leader>bo", ":BufferLineCloseOthers<CR>", opt)
-map("n", "<leader>br", ":BufferLineCloseRight<CR>", opt)
-map("n", "<leader>bl", ":BufferLineCloseLeft<CR>", opt)
+vim.keymap.set("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+vim.keymap.set("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+vim.keymap.set("n", "<leader>bo", ":BufferLineCloseOthers<CR>", opt)
+vim.keymap.set("n", "<leader>br", ":BufferLineCloseRight<CR>", opt)
+vim.keymap.set("n", "<leader>bl", ":BufferLineCloseLeft<CR>", opt)
 
 vim.keymap.set("n", "<leader>bx", function()
 	require("snacks").bufdelete()
 end, { desc = "关闭buf" })
 
 --注释
-map("n", "<leader>/", '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', opt)
-map("v", "<leader>/", '<ESC><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', opt)
+vim.keymap.set("n", "<leader>/", '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', opt)
+vim.keymap.set("v", "<leader>/", '<ESC><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', opt)
 
 -- 使用黑洞寄存器，不替换剪贴版内容
-map("v", "d", '"_d', opt)
-map("x", "d", '"_d', opt)
-map("v", "c", '"_c', opt)
-map("x", "c", '"_c', opt)
+vim.keymap.set("n", "<leader>dd", '"_dd', opt)
+vim.keymap.set("n", "D", '"_D', opt)
+vim.keymap.set("v", "d", '"_d', opt)
+vim.keymap.set("x", "d", '"_d', opt)
+vim.keymap.set("v", "c", '"_c', opt)
+vim.keymap.set("x", "c", '"_c', opt)
 
-map("n", "<leader>n", ":lua require('dapui').toggle()<CR>", opt)
+vim.keymap.set("n", "<leader>n", ":lua require('dapui').toggle()<CR>", opt)
 
 -- gitsigns 相关
-map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", opt)
-map("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", opt)
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", opt)
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", opt)
 
 -- iOS 设置
 vim.keymap.set("n", "<A-d>", "<cmd>silent cc | silent cn<cr>zz", { desc = "Jump to next issue" })
