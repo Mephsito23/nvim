2cac4fbf8cbc69db753243cfea9d9343cde8169e
Mephisto <xiezefu@outlook.com>
2026-06-01 17:28:07 +0800

    fix:修复新版tressitter 不下载对应语言颜色文件bug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,35 @@
+local parsers = {
+	"lua",
+	"swift",
+	"python",
+	"rust",
+	"go",
+	"gomod",
+	"gowork",
+	"gosum",
+	"javascript",
+	"typescript",
+	"html",
+	"css",
+	"json",
+	"gdscript",
+	"godot_resource",
+	"gdshader",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		priority = 1000,
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"lua",
-				"swift",
-				"python",
-				"rust",
-				"go",
-				"gomod",
-				"gowork",
-				"gosum",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"json",
-				"gdscript",
-				"godot_resource",
-				"gdshader",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-			auto_install = false,
-			fold = { enable = true },
-			enable = true,
-			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit
-			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-			line_numbers = true,
-			multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-			-- Separator between context and content. Should be a single character string, like '-'.
-			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-			separator = nil,
-			zindex = 20, -- The Z-index of the context window
-			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-		},
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			require("nvim-treesitter").install(parsers)
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
